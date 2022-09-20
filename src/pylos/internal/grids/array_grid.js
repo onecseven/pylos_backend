5e17f35c89e72d68b46531a3678c5546b3eeb477
Your Name <iced.tati@gmail.com>
2022-09-20 13:19:30 -0300

    falling to pressure

--- src/pylos/internal/grids/array_grid.js
+++ src/pylos/internal/grids/array_grid.js
@@ -2,14 +2,15 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.ArrayGrid = void 0;
 const token_js_1 = require("../token.js");
+//travesty but necessary
 class ArrayGrid {
     constructor(w) {
         this.grid = new Array(w).fill(undefined).map(v => new token_js_1.Token("EMPTY"));
     }
-    get_tile(x) {
+    get_tile(x, y) {
         return this.grid[x];
     }
-    set_tile(x, value) {
+    set_tile(x, y, value) {
         this.grid[x] = value;
     }
 }
