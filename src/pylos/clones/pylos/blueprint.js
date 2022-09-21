f767bddb0fb8811fd26bd80b0d93386fe90893e0
Your Name <iced.tati@gmail.com>
2022-09-21 13:20:24 -0300

    getting rid of tests and a minor win con

--- src/pylos/clones/pylos/blueprint.js
+++ src/pylos/clones/pylos/blueprint.js
@@ -5,9 +5,20 @@ const token_js_1 = require("../../internal/token.js");
 const phase_js_1 = require("../../internal/phase.js");
 const reducer_js_1 = require("./reducer.js");
 const Game_State_Machine_js_1 = require("../../internal/Game_State_Machine.js");
+const moves_js_1 = require("./moves.js");
 const zone_js_1 = require("../../internal/zone.js");
 let win_con_check = (state) => {
     var _a, _b;
+    // for (let player_index in state.players) {
+    //   let player = state.players[player_index]
+    //   let opponent = 1 - Number(player_index)
+    //   if (player.BALLS.length === 0) {
+    //     return {
+    //       win_condition_is_met: true,
+    //       winner: opponent,
+    //     }
+    //   }
+    // }
     let winning_zone = state.zones[3];
     let ball_exists = ((_a = winning_zone.grid.get_tile(0, 0)) === null || _a === void 0 ? void 0 : _a.type) === "BALL";
     let owner = (_b = winning_zone.grid.get_tile(0, 0)) === null || _b === void 0 ? void 0 : _b.owner;
@@ -62,14 +73,13 @@ let game_state = () => Game_State_Machine_js_1.GSM.decorator({
 }, win_con_check);
 let create_pylos_game = () => new Game_State_Machine_js_1.GSM(game_state());
 exports.create_pylos_game = create_pylos_game;
-// let pylos = create_pylos_game()
-// let pylos = new GSM(game_state())
+let pylos = new Game_State_Machine_js_1.GSM(game_state());
 // let pylos_2 = create_pylos_game()
-// let moves_1 = PLACE(0, { x: 0, y: 3, z: 0 })
+let moves_1 = (0, moves_js_1.PLACE)(0, { x: 0, y: 3, z: 0 });
 // let moves_3 = PLACE(0, helpers.create_coords(1, 1, 0))
 // let moves_4 = PLACE(1, helpers.create_coords(1, 0, 0))
 // let moves_5 = PLACE(0, helpers.create_coords(2, 0, 0))
-// pylos.send_move(moves_1)
+pylos.send_move(moves_1);
 // pylos.send_move(moves_3)
 // pylos.send_move(moves_4)
 // pylos.send_move(moves_5)
