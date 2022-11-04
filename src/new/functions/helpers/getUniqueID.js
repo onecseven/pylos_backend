36991a0e09bc8271681320ba96ebaa1ecb90dff7
Your Name <iced.tati@gmail.com>
2022-11-04 20:01:52 -0300

    helpers

---
+++ src/new/functions/helpers/getUniqueID.js
@@ -0,0 +1,7 @@
+function getUniqueID() {
+    function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+    }
+    return s4() + s4() + '-' + s4();
+}
+exports.getUniqueID = getUniqueID;
