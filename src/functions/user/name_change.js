62e2ee46194a3684de4ee0dc43f851ccb398c16e
Your Name <iced.tati@gmail.com>
2022-11-06 14:59:32 -0300

    s

--- src/functions/user/name_change.js
+++ src/functions/user/name_change.js
@@ -1,9 +1,11 @@
-const messages = require("../../messages")
-
+const messages = require("../messages")
+const send_data = require("../switchboard/send_to_user")
 const change_name = (user, data) => {
   let { name } = data
   if (name.length >= 4 && name.length <= 10) user.name = name
-  user.sendData(messages.YOUR_ID(user.id, user.name))
+  send_data(messages.YOUR_ID(user.id, user.name), user.id)
 }
 
 module.exports = change_name
+
+//fa3d4189-b407
