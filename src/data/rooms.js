89a0a57a584b0ce0a8338bee5deb1dc5f337522a
Your Name <iced.tati@gmail.com>
2022-11-06 14:58:39 -0300

    new room

--- src/data/rooms.js
+++ src/data/rooms.js
@@ -1,1 +1,79 @@
-module.exports = {}
+const englishWordGen = require("../functions/helpers/wordGen")
+const storage = require("./storage").init()
+const deserialize_move_list = require("../functions/games/deserialize_move_list")
+class Room {
+  id = englishWordGen()[0]
+  users = []
+  maxUsers = 2
+  game = null
+
+  constructor() {}
+
+  join(user) {
+    if (
+      this.maxUsers > room.users.length &&
+      !room.started &&
+      room.users.indexOf(user) == -1
+    ) {
+      this.users.push(user)
+      return true
+    } else {
+      return false
+    }
+  }
+
+  isHost(user) {
+    return this.users.indexOf(user) == 0
+  }
+
+  hasUser(user) {
+    return this.users.indexOf(user) !== -1
+  }
+
+  getHost() {
+    return this.users[0]
+  }
+
+  serialize() {
+    return {
+      id: this.id,
+      users: this.users.map((user) => user.serialize()),
+      maxUsers: this.maxUsers,
+      game: this.game ? this.game.serial_state.move_history : null,
+    }
+  }
+
+  static deserialize(serializedRoom) {
+    let temp = new Room()
+    
+    if (serializedRoom.game) {
+      let temp_game = deserialize_move_list(serializedRoom.game)
+      serializedRoom.game = temp_game
+    }
+    Object.assign(temp, serializedRoom)
+    return temp
+  }
+}
+
+class Rooms {
+  rooms = new Map()
+
+  get(id) {
+    if (this.rooms.has(id)) return this.rooms.get(id)
+    return null
+  }
+
+  create() {
+    let room = new Room()
+    this.rooms.add(room)
+    return room
+  }
+
+  add(room) {
+    this.rooms.set(room.id, room)
+  }
+}
+
+let singleton = new Rooms()
+
+module.exports = singleton
