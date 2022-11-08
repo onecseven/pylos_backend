3dce36a3bd0d18a80615c205010d4d50dda1d2dd
Your Name <iced.tati@gmail.com>
2022-11-08 14:59:27 -0300

    new functions

--- src/functions/index.js
+++ src/functions/index.js
@@ -5,9 +5,11 @@ const leave_room = require("./room/leave_room")
 const start_pylos = require("./games/start_pylos")
 const process_move = require("./games/process_move")
 const user_wants_rematch = require("./room/user_wants_rematch")
+const get_rooms = require("./user/get_rooms")
 // const create_user = require("./user/create_user")
 const remove_user = require("./user/user_disconnected")
 const login = require("./user/login")
+const rejoin = require("./room/rejoin")
 
 module.exports = {
   room: {
@@ -15,9 +17,11 @@ module.exports = {
     create_room,
     leave_room,
     user_wants_rematch,
+    rejoin
   },
   user: {
     name_change,
+    get_rooms,
     // create_user,
     remove_user,
     login
