27454aea78b134590b64d8641794a4dd4b081094
Your Name <iced.tati@gmail.com>
2022-11-08 14:59:45 -0300

    idk

--- src/functions/games/process_move.js
+++ src/functions/games/process_move.js
@@ -4,18 +4,20 @@ const send_data_to_everyone = require("../switchboard/send_to_everyone")
 const wincon_check = require("./wincon_check")
 
 let distribute_state = (state, roomId) => send_data_to_everyone(messages.GAME_STATE(state), roomId)
-let failed_move = (error, roomId) => send_data_to_everyone(messages.MOVE_FAILED(state), roomId)
+let failed_move = (error, roomId) => send_data_to_everyone(messages.MOVE_FAILED(error), roomId)
 
 
 const process_pylos_move = (user, data) => {
   let id = data.gameId
   let move = data.move
 
-  let room = rooms.get
+  let room = rooms.get(id)
+  if (!room) return console.error("Move on non-room")
+  
   let game = room.game
 
   let current_player = game.serial_state.current_player
-  let sender_turn_num = room.users.indexOf(user)
+  let sender_turn_num = room.users.map(u => u.id).indexOf(user.id)
   let move_player = move.player
   
   let checks = (move_player === sender_turn_num &&
