b334b91560c4315548c8985e33104663dc98cbcf
Your Name <iced.tati@gmail.com>
2022-11-15 18:05:28 -0300

    s

--- src/data/db/db.js
+++ src/data/db/db.js
@@ -4,5 +4,8 @@ module.exports = new Sequelize({
   dialect: "sqlite",
   // storage: "./DB.sqlite3",
   storage: "D:/backup/code/gameroom-backend/src/data/db/DB.sqlite3",
+  define: {
+    timestamps: false
+  }
 })
 
