c4d763ac3d16dabce74ef4d650ed36a8475e54c2
Your Name <iced.tati@gmail.com>
2022-11-15 20:04:41 -0300

    eh

--- src/functions/switchboard/send_to_everyone.js
+++ src/functions/switchboard/send_to_everyone.js
@@ -5,9 +5,9 @@ const send_to_room = (data, roomId) => {
   rooms_db
     .get_users_on_room(roomId)
     .then((users) => {
-      for (let user of users) {
+      users.forEach(({user_id}) => {
         send_to_user(data, user.user_id)
-      }
+      })
     })
     .catch((e) => console.log("Send to room failed."))
 }
