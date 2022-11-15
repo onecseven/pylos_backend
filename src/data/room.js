22db820ebf173669c002ba1c442ce6c5cbaa81c7
Your Name <iced.tati@gmail.com>
2022-11-15 19:59:46 -0300

    do not like orms

--- src/data/room.js
+++ src/data/room.js
@@ -20,7 +20,7 @@ const create_room = async ({ room_id, max_users = 2, host, game = null }) => {
   try {
     let room = await Room.create({ room_id, max_users, host, game, rematch: 0 })
     if (room) {
-      console.log("Room Created: ", JSON.stringify(room, null, 2))
+      console.log("Room Created: ", room.room_id)
       let user = await get_user_by_id(host)
       room.addUser(user)
     }
@@ -41,7 +41,7 @@ const add_user_to_room = async (room_id, user_id) => {
   try {
     let room = await get_room_by_id(room_id)
     let user = await get_user_by_id(user_id)
-    if (room && user) room.addUser([user])
+    if (room && user) await room.addUser([user])
   } catch (e) {
     console.error(e)
   }
@@ -80,7 +80,7 @@ const get_room_by_id = async (room_id) => {
         room_id,
       },
     })
-    console.log("Got Room: ", JSON.stringify(room, null, 2))
+    console.log("Got Room: ", room.room_id)
     return room
   } catch (e) {
     console.error(e)
@@ -92,15 +92,19 @@ const get_room_by_id = async (room_id) => {
  * @returns {string[]}
  */
 const get_users_on_room = async (room_id) => {
+  //lmao get fucked sequelize... i dont have to "associate" shit
+  let query_string =
+    "SELECT `user`.`user_id`, `user`.`name`, `room_users`.`updatedAt` AS `room_users.updatedAt`, `room_users`.`userUserId` AS `room_users.userUserId`, `room_users`.`roomRoomId` AS `room_users.roomRoomId` FROM `users` AS `user` INNER JOIN `room_users` AS `room_users` ON `user`.`user_id` = `room_users`.`userUserId` AND `room_users`.`roomRoomId` =" +
+    "'" +
+    room_id +
+    "'" +
+    " ORDER BY `room_users`.`updatedAt` ASC;"
   await db.sync()
   try {
-    let users = await RoomUsers.findAll({
-      where: {
-        roomRoomId: room_id,
-      },
-      order: [["updatedAt", "ASC"]],
-    })
-    return users?.map(({ userUserId, roomRoomId }) => userUserId)
+    let [results, meta] = await db.query(query_string)
+    if (results.length) {
+      return results.map(({ user_id, name }) => ({ user_id, name }))
+    }
   } catch (e) {
     console.error(e)
   }
@@ -108,19 +112,17 @@ const get_users_on_room = async (room_id) => {
 
 ;(async () => {
   // await create_user({ user_id: "notati", name: "rungen" })
-  await create_user({ user_id: "sitati", name: "rongen" })
+  // await create_user({ user_id: "errorwithcreate", name: "isittrue" })
   // let x =  await get_room_by_id("loul2")
   // let x = await create_room({ room_id: "loul2", host: "notati" })
   // let y = x.hasUser()
-  await add_user_to_room("loul2", "sitati")
+  // await add_user_to_room("loul2", "sitati")
   // console.log( x.hasUser("notati"))
   // x.rematch = 1
   // x.save()
   // await add_user_to_room("lol", "notati")
-  // await get_users_on_room("lol")
   // await update_game_in_room("lol", JSON.stringify(["hi","hey", "how are you"]))
-  let x = await get_users_on_room("loul2")
-  console.log(x)
+  // let x = await get_users_on_room("loul2")
 })()
 
 module.exports = {
