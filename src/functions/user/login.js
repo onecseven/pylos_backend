c71574291a6d28162c21b715419289881ad37a5e
Your Name <iced.tati@gmail.com>
2022-11-15 12:50:15 -0300

    work

--- src/functions/user/login.js
+++ src/functions/user/login.js
@@ -8,14 +8,14 @@ const login = async (user, data) => {
   let actual_id = data.id
   let old_id = user.id
   let connection = switchboard.get(old_id)
-  let db_user = await user_db.get_user_by_id(user.id)
+  let db_user = await user_db.get_user_by_id(actual_id)
   if (!db_user) {
     send_data(messages.MOVE_FAILED("ID not found"), old_id)
   }
   user.name = db_user.name
   user.id = db_user.user_id
   switchboard.add(actual_id, connection)
-  send_data(messages.YOUR_ID(actual_id, new_user.name), actual_id)
+  send_data(messages.YOUR_ID(actual_id, user.name), actual_id)
 }
 
 module.exports = login
--- src/functions/user/name_change.js
+++ src/functions/user/name_change.js
@@ -1,18 +1,19 @@
 const messages = require("../messages")
 const send_data = require("../switchboard/send_to_user")
 const user_db = require("../../data/user")
+const save_user = require("./save_user")
 
 const change_name = async (user, data) => {
   let { name } = data
-  if (name.length >= 4 && name.length <= 20) {
+  if (name.length <= 4 && name.length >= 20) return send_data(messages.MOVE_FAILED("Name invalid"), user.id)
+  let dbuser = user_db.get_user_by_id(user.id)
+  if (dbuser) {
     let result = await user_db.update_user(user.id, name)
-    if (result) {
-      send_data(messages.YOUR_ID(user.id, user.name), user.id)
-      user.name = data.name
-    } else {
-      send_data(messages.MOVE_FAILED("Name change did not happen"), user.id)
-    }
+    if (!result) console.error("Couldn't name change the following user", JSON.stringify(user, null, 2))
   }
+  user.name = data.name
+  await save_user(user)
+  send_data(messages.YOUR_ID(user.id, user.name), user.id)
 }
 
 module.exports = change_name
