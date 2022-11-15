0fe30a0bb3689d2048900205478a09ad4497868a
Your Name <iced.tati@gmail.com>
2022-11-15 18:06:12 -0300

    small improvements

--- src/functions/room/rejoin.js
+++ src/functions/room/rejoin.js
@@ -1,22 +1,25 @@
-const rooms  = require("../../data/rooms")
+const rooms_db = require("../../data/room")
 const messages = require("../messages")
 const send_data = require("../switchboard/send_to_user")
-const send_to_room = require("../switchboard/send_to_everyone")
 
 const rejoin = async (user, data) => {
   let { roomId } = data
-  let room = rooms.get(roomId)
+  let room = await rooms_db.get_room_by_id(roomId)
+
   if (!room) {
-    let storage = await rooms.stored_lookup(roomId)
-    if (storage) room = storage
-    else return
+    return null //message?
   }
 
-  if (room && room?.hasUser(user.id) && room?.join(user)) {
-    send_data(messages.JOINED_ROOM(room.id, room.users, room.users[0].id), user.id)
+  if (room?.hasUser(user.id)) {
+    let users = await rooms_db.get_users_on_room(roomId)
+    if (!users) {
+      return null //message?
+    }
+
+    send_data(messages.JOINED_ROOM(room.room_id, users, room.host), user.id)
     send_data(messages.GAME_STARTED(room.id), user.id)
-    room.users.forEach((u, i) => {
-      if (u.id === user.id) {
+    users.forEach((u, i) => {
+      if (u === user.id) {
         send_data(messages.GIVE_TURN_ORDER(i), user.id)
       }
     })
--- src/functions/switchboard/send_to_everyone.js
+++ src/functions/switchboard/send_to_everyone.js
@@ -1,11 +1,15 @@
-const { rooms } = require("../../data/rooms")
+const rooms_db = require("../../data/room")
 const send_to_user = require("./send_to_user")
 
 const send_to_room = (data, roomId) => {
-  let room = rooms.get(roomId)
-  if (room) {
-    room.users.forEach((user) => send_to_user(data, user.id))
-  }
+  rooms_db
+    .get_users_on_room(roomId)
+    .then((users) => {
+      for (let user_id of users) {
+        send_to_user(data, user_id)
+      }
+    })
+    .catch((e) => console.log("Send to room failed."))
 }
 
 module.exports = send_to_room
