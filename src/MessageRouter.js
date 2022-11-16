e744e48b0ad0eec027828afede9484f7e6bb54dc
Your Name <iced.tati@gmail.com>
2022-11-16 20:05:36 -0300

    testing game funcs

--- src/MessageRouter.js
+++ src/MessageRouter.js
@@ -1,12 +1,12 @@
-const {room, user, game} = require("./functions/index")
+const {room, user, game} = require("./functions/")
 
 let cases = {
   "my name is": user.name_change,
   "join room": room.join_room,
   "create room": room.create_room,
   // "leave room": room.leave_room,
-  // "start pylos game": game.start_pylos,
-  // "pylos move": game.process_move,
+  "start pylos game": game.start_pylos,
+  "pylos move": game.process_move,
   "user wants rematch": room.user_wants_rematch,
   "my rooms": user.get_rooms,
   "login": user.login,
--- src/functions/games/start_pylos.js
+++ src/functions/games/start_pylos.js
@@ -1,35 +1,40 @@
-const rooms = require("../../data/rooms")
+const rooms_db = require("../../data/room")
 const { create_pylos_game } = require("./pylos/clones/pylos/blueprint")
 const messages = require("../messages")
 const send_data = require("../switchboard/send_to_user")
 const send_to_room = require("../switchboard/send_to_everyone")
+const switchboard = require("../../data/Switchboard")
 
-const not_enough = (user) =>
+const not_enough = async (user) =>
   send_data(messages.GAME_START_FAIL("not enough players"), user.id)
 const game_started = (id) => send_to_room(messages.GAME_STARTED(id), id)
 const send_turn = (userid, turn_id) =>
   send_data(messages.GIVE_TURN_ORDER(turn_id), userid)
-const distribute_state = (state, roomId) => send_to_room(messages.GAME_STATE(state), roomId)
+const distribute_state = (state, roomId) =>
+  send_to_room(messages.GAME_STATE(state), roomId)
 
-const start_pylos = (user, data) => {
-  let id = data.gameId
-  let room = rooms.get(id)
+const start_pylos = async (user, data) => {
+  let room_id = data.gameId
+  let room = await rooms_db.get_room_with_users(room_id)
+
   if (!room) {
-    console.error("Trying to start pylos on an inexistent room.")
-    return
+    return console.error(
+      "Trying to start pylos on an inexistent room (",
+      + room_id + ")."
+    )
+  } else if (room.users.length < 2) {
+    return not_enough(user)
+  } else if (room.host !== user.id) {
+    return console.error("Not host trying to start game.")
   }
-
-  if (room.users.length !== 2) {
-    not_enough(user)
-    return
-  }
-
-  if (room.isHost(user.id)) {
-    game_started(room.id)
-    room.users.forEach((user, i) => send_turn(user.id, i))
-    room.game = create_pylos_game()
-    distribute_state(room.game.serial_state, room.id)
-  }
+  
+  game_started(room_id)
+  room.users.forEach((user, i) => send_turn(user.user_id, i))
+  room.update({ game: "[]" })
+  room.save()
+  switchboard.add_game(room_id, create_pylos_game())
+  let game = switchboard.get_game(room_id)
+  distribute_state(game.serial_state, room_id)
 }
 
 module.exports = start_pylos
