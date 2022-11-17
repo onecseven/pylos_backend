0f0fbae534166acd2d3c71b396e787d09a7195be
Your Name <iced.tati@gmail.com>
2022-11-17 19:44:09 -0300

    working

--- src/functions/room/rejoin.js
+++ src/functions/room/rejoin.js
@@ -1,29 +1,34 @@
 const rooms_db = require("../../data/room")
 const messages = require("../messages")
 const send_data = require("../switchboard/send_to_user")
+const switchboard = require("../../data/Switchboard")
 
-const rejoin = async (user, data) => {
+const rejoin = async (user, data) => { //working
   let { roomId } = data
-  let room = await rooms_db.get_room_by_id(roomId)
-
+  let room = await rooms_db.get_room_with_users(roomId)
+  
   if (!room) {
     return null //message?
   }
 
-  if (room?.hasUser(user.id)) {
-    let users = await rooms_db.get_users_on_room(roomId)
-    if (!users) {
-      return null //message?
-    }
-    //check that a game is going on
-    send_data(messages.JOINED_ROOM(room.room_id, users, room.host), user.id)
-    send_data(messages.GAME_STARTED(room.id), user.id)
-    users.forEach((u, i) => {
-      if (u === user.id) {
+  if (room.users.find(iser => iser.user_id === user.id)) {
+    let game = await switchboard.get_game(roomId, { rehydrate: true })
+    if (!game)
+      return console.error(
+        "Trying tor reenter a room with no game",
+        JSON.stringify(data, null, 2)
+      )
+    send_data(
+      messages.JOINED_ROOM(room.room_id, room.users, room.host),
+      user.id
+    )
+    send_data(messages.GAME_STARTED(room.room_id), user.id)
+    room.users.forEach((u, i) => {
+      if (u.user_id === user.id) {
         send_data(messages.GIVE_TURN_ORDER(i), user.id)
       }
     })
-    send_data(messages.GAME_STATE(room.game.serial_state), user.id)
+    send_data(messages.GAME_STATE(game.serial_state), user.id)
   }
 }
 
--- src/functions/user/login.js
+++ src/functions/user/login.js
@@ -11,9 +11,11 @@ const login = async (user, data) => {
   if (!db_user) {
     send_data(messages.ERROR("ID not found"), old_id)
     user.id = actual_id
+    user.user_id = actual_id
   } else {
     user.name = db_user.name
     user.id = db_user.user_id
+    user.user_id = db_user.user_id
   }
   switchboard.add(actual_id, connection)
   send_data(messages.YOUR_ID(actual_id, user.name), actual_id)
